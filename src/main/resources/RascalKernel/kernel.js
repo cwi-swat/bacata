edc519198648b7daf8ab67ea0731d689e459a02c
Mauricio Verano Merino <mauricioveranom@me.com>
2017-06-09 14:55:54 +0200

    first commit

---
+++ src/main/resources/RascalKernel/kernel.js
@@ -0,0 +1,9 @@
+define(function(){
+  return {onload: function(){
+    console.info('Rascal kernel specific javascript loaded');
+
+    // do more things here, like define a codemirror mode
+
+  }}
+
+});
