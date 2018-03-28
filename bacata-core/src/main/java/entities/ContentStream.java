1b3eadbd740163d9accaaf7f850dc340a125dbf2
Mauricio Verano Merino <m.verano239@gmail.com>
2018-03-28 16:00:50 +0200

    some constants added to the bacata-core

--- bacata-core/src/main/java/entities/ContentStream.java
+++ bacata-core/src/main/java/entities/ContentStream.java
@@ -3,12 +3,20 @@ package entities;
 import entities.util.Content;
 
 public class ContentStream extends Content{
+
+	// -----------------------------------------------------------------
+	// Constants
+	// -----------------------------------------------------------------
+	public final static String STD_ERR = "stderr";
+	public final static String STD_OUT = "stdout";
 	
 	// -----------------------------------------------------------------
 	// Fields
 	// -----------------------------------------------------------------
+	@SuppressWarnings("unused")
 	private String name;
 	
+	@SuppressWarnings("unused")
 	private String text;
 
 	// -----------------------------------------------------------------
