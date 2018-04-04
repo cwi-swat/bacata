eff4b8126932e2822dbb318a91b5d9e488dc016f
Mauricio Verano Merino <m.verano239@gmail.com>
2018-04-04 15:42:27 +0200

    rascal kernel.json updated to be used with docker and kernel.js updated

---
+++ bacata-rascal/src/bacata/Deploy.rsc
@@ -0,0 +1,10 @@
+module bacata::Deploy
+
+void deployNotebook(loc projectPath){
+
+}
+
+str dockerFile =
+"
+"
+;
--- bacata-rascal/src/bacata/kernel/rascal/kernel.js
+++ bacata-rascal/src/bacata/kernel/rascal/kernel.js
@@ -1,7 +1,6 @@
 define([
 	'custom/salix'], function(){
   		return {onload: function(){
-			$(document).ready(new Salix().start);
   		}
 		};
 });
