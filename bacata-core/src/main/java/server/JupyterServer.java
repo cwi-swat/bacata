828b490bc36ba29fc659213bf33a97e6e2288a1b
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2021-01-21 09:19:49 +0100

    trying to start kernel sooner

--- bacata-core/src/main/java/server/JupyterServer.java
+++ bacata-core/src/main/java/server/JupyterServer.java
@@ -118,6 +118,9 @@ public abstract class JupyterServer {
 			poller.register(communication.getIOPubSocket(), ZMQ.Poller.POLLIN);
 			poller.register(communication.getHeartbeatSocket(), ZMQ.Poller.POLLIN);
 			
+			// see if this wakes up the kernel sooner
+			heartbeatChannel();
+
 			while (!Thread.currentThread().isInterrupted()) {
 				poller.poll();
 				if (poller.pollin(0)) {
