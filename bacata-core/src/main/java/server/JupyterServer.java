e56c7c7da6435d1cb4fc8ce397544990d8cf5ea8
Mauricio Verano Merino <m.verano239@gmail.com>
2021-01-31 23:18:46 +0100

    Reverted changes

--- bacata-core/src/main/java/server/JupyterServer.java
+++ bacata-core/src/main/java/server/JupyterServer.java
@@ -174,11 +174,9 @@ public class JupyterServer {
 		Header header, parentHeader = message.getHeader(); // Parent header for the reply.
 		switch (parentHeader.getMsgType()) {
 			case MessageType.KERNEL_INFO_REQUEST:
-//				statusUpdate(message.getHeader(), Status.STARTING);
 				header = new Header(MessageType.KERNEL_INFO_REPLY, parentHeader);
 				contentReply = (ContentKernelInfoReply) processKernelInfoRequest(message);
 				sendMessage(communication.getShellSocket(), header, parentHeader, contentReply);
-				statusUpdate(message.getHeader(), Status.IDLE);
 				break;
 			case MessageType.SHUTDOWN_REQUEST:
 				header = new Header(MessageType.SHUTDOWN_REPLY, parentHeader);
