5549e7a04fdc71aee8294a91d5cc1bd0a4ab4944
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2021-02-08 10:09:35 +0100

    fixed off-by-one error

--- bacata-core/src/main/java/server/JupyterServer.java
+++ bacata-core/src/main/java/server/JupyterServer.java
@@ -299,7 +299,7 @@ public class JupyterServer {
 			suggestions = result.getSuggestions().stream().collect(Collectors.toList());
 		}
 		
-		Content reply = new ContentCompleteReply(suggestions, content.getCursorPosition(), content.getCode().length(), new HashMap<String, String>(), Status.OK);
+		Content reply = new ContentCompleteReply(suggestions, content.getCursorPosition() - 1, content.getCode().length(), new HashMap<String, String>(), Status.OK);
 
 		sendMessage(
 			communication.getShellSocket(), 
