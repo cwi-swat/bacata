f4e9553a3975625e628088c3d0dfe89447a75691
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2021-02-18 18:49:10 +0100

    parameterized version

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -5,4 +5,4 @@ mvn clean install
 
 VERSION=` mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout`
 
-docker image build -f rascal-notebook-docker/Dockerfile -t bacata:${VERSION} .
+docker image build --build-arg rascalNotebookVersion=${VERSION} -f rascal-notebook-docker/Dockerfile -t bacata:${VERSION} .
